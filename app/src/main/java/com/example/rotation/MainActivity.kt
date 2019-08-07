7b36b066eeb25ea62d00ceea340cc03ea56f950b
sun-yryr <taittide@gmail.com>
2019-08-07 16:11:49 +0900

    地磁気と加速度から回転角，回転行列の生成

--- app/src/main/java/com/example/rotation/MainActivity.kt
+++ app/src/main/java/com/example/rotation/MainActivity.kt
@@ -8,6 +8,7 @@ import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Switch
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileOutputStream
@@ -21,6 +22,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private var mSensor: Sensor by Delegates.notNull<Sensor>()
     private var mgSensor: Sensor by Delegates.notNull<Sensor>()
 
+
+    private val MATRIX_SIZE = 16
+    private val DIMENSION = 3
+    private var mMagneticValues = FloatArray(3)
+    private var mAccelerometerValues = FloatArray(3)
+
+
     var time = 0.0
     val epsiron = 0.000001
 
@@ -28,8 +36,10 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         mManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        mSensor = mManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        mSensor = mManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        mgSensor = mManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
     }
 
     fun cast(a: Double): Double {
@@ -40,17 +50,38 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
+    fun toDegrees(values: FloatArray): FloatArray {
+        values.map { value ->
+            if (value >= 0) {
+                Math.floor(Math.toDegrees(value.toDouble()))
+            } else {
+                Math.floor(Math.toDegrees(value.toDouble()))
+            }
+        }
+        return values
+    }
+
     override fun onSensorChanged(event: SensorEvent) {
-        if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
-            val now = event.timestamp / pow(10.0, 9.0)
-            val x = cast(event.values[0]*(now - time))
-            val y = cast(event.values[1]*(now - time))
-            val z = cast(event.values[2]*(now - time))
-            // Log.d("test", "X: "+x.toString()+", Y: "+y.toString()+", Z: "+z.toString())
-            Log.d("test:x", x.toString())
-            Log.d("test:y", y.toString())
-            Log.d("test:z", z.toString())
-            this.time = now
+        when (event.sensor.type) {
+            Sensor.TYPE_ACCELEROMETER -> {
+                mAccelerometerValues = event.values.clone()
+            }
+            Sensor.TYPE_MAGNETIC_FIELD -> {
+                mMagneticValues = event.values.clone()
+            }
+        }
+        if (mAccelerometerValues.isNotEmpty() && mMagneticValues.isNotEmpty()) {
+            var rotationMatrix = FloatArray(MATRIX_SIZE)
+            var inclinationMatrix = FloatArray(MATRIX_SIZE)
+            var remapedMatrix = FloatArray(MATRIX_SIZE)
+            var orientationValues = FloatArray(DIMENSION)
+
+            SensorManager.getRotationMatrix(rotationMatrix, inclinationMatrix, mAccelerometerValues, mMagneticValues)
+            SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Z, remapedMatrix)
+            SensorManager.getOrientation(remapedMatrix, orientationValues)
+
+            var degs = toDegrees(orientationValues)
+
         }
     }
 
@@ -72,6 +103,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         //第二引数は取得したセンサーオブジェクト
         //第三引数は更新頻度 UIはUI表示向き、FASTはできるだけ早く、GAMEはゲーム向き
         mManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_FASTEST)
-        //mManager.registerListener(this, gSensor, SensorManager.SENSOR_DELAY_FASTEST)
+        mManager.registerListener(this, mgSensor, SensorManager.SENSOR_DELAY_FASTEST)
     }
 }
--- app/src/main/java/com/example/rotation/gravity.kt
+++ app/src/main/java/com/example/rotation/gravity.kt
@@ -2,6 +2,3 @@ package com.example.rotation
 
 import android.hardware.SensorEvent
 
-public fun gravityToRotate(sensor: SensorEvent): Array<Double> {
-
-}
